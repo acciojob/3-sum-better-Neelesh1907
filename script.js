c893b0a17495848be9b252a1e61c174ec3a988ee
Neelesh1907 <119998532+Neelesh1907@users.noreply.github.com>
2023-09-11 18:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let minDiff=0,c,d,e;
+	for(let i=0;i<ar.length;i++){
+		if(i>0 && arr[i]===arr[i-1]){
+			continue;
+		}
+		let j = i+1,k=arr.length-1;
+		while(j<k){
+			
+		   let x = arr[i]+arr[j]+arr[k];
+		   diff = x>target?(x-target):(target-x);
+		   if(minDiff>diff){
+			  c=ar[i];
+			  d=ar[j];
+			  e=ar[k];
+		   }
+		   j++;
+		   k--;
+		}
+	}
+	return c,d,e;
 }
 
 module.exports = threeSum;
