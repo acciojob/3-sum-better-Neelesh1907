05356e55208d5db382bf5070062a5f8c53f695d9
Neelesh1907 <119998532+Neelesh1907@users.noreply.github.com>
2023-09-11 19:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
 function threeSum(arr, target) {
 // write your code here
-	// arr.sort((a,b)=>a-b);
-	// let minDiff=0,c,d,e;
-	// for(let i=0;i<arr.length-2;i++){
-	// 	if(i>0 && arr[i]===arr[i-1]){
-	// 		continue;
-	// 	}
-	// 	let j = i+1,k=arr.length-1;
-	// 	while(j<k){
+	arr.sort((a,b)=>a-b);
+	let minDiff=0,c=0,d=0,e=0;
+	for(let i=0;i<arr.length-2;i++){
+		if(i>0 && arr[i]===arr[i-1]){
+			continue;
+		}
+		let j = i+1,k=arr.length-1;
+		while(j<k){
 			
-	// 	   let x = arr[i]+arr[j]+arr[k];
-	// 	   diff = x>target?(x-target):(target-x);
-	// 	   if(minDiff>diff){
-	// 		  c=ar[i];
-	// 		  d=ar[j];
-	// 		  e=ar[k];
-	// 		  minDiff=diff; 
-	// 	   }
-	// 	   if(x<target){
-	// 		   j++;
-	// 	   }
-	// 	   else{
-	// 	       k--;	
-	// 	  }	
-	// 	}
-	// }
-	// return (c+d+e);
-	arr.sort((a, b) => a - b);
+		   let x = arr[i]+arr[j]+arr[k];
+		   diff = x>target?(x-target):(target-x);
+		   if(minDiff>diff){
+			  c=ar[i];
+			  d=ar[j];
+			  e=ar[k];
+			  minDiff=diff; 
+		   }
+		   if(x<target){
+			   j++;
+		   }
+		   else{
+		       k--;	
+		  }	
+		}
+	}
+	return (c+d+e);
+	// arr.sort((a, b) => a - b);
   
-  // Initialize variables
-  let closestSum = arr[0] + arr[1] + arr[2];
+ //  // Initialize variables
+ //  let closestSum = arr[0] + arr[1] + arr[2];
   
-  // Iterate through the array
-  for (let i = 0; i < arr.length - 2; i++) {
-    let left = i + 1;
-    let right = arr.length - 1;
+ //  // Iterate through the array
+ //  for (let i = 0; i < arr.length - 2; i++) {
+ //    let left = i + 1;
+ //    let right = arr.length - 1;
     
-    while (left < right) {
-      const sum = arr[i] + arr[left] + arr[right];
+ //    while (left < right) {
+ //      const sum = arr[i] + arr[left] + arr[right];
       
-      // Check if the current sum is closer to the target
-      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-        closestSum = sum;
-      }
+ //      // Check if the current sum is closer to the target
+ //      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+ //        closestSum = sum;
+ //      }
       
-      if (sum < target) {
-        left++;
-      } else if (sum > target) {
-        right--;
-      } else {
-        // Found an exact match, so return the sum
-        return sum;
-      }
-    }
-  }
+ //      if (sum < target) {
+ //        left++;
+ //      } else if (sum > target) {
+ //        right--;
+ //      } else {
+ //        // Found an exact match, so return the sum
+ //        return sum;
+ //      }
+ //    }
+ //  }
   
-  return closestSum;
+ //  return closestSum;
 }
 
 module.exports = threeSum;
